b15c2491cea6dc7e7bffd0f7fd4cf3c313c7814d
Muhamad Istiady Kartadibrata <mkartadibrata@gmail.com>
2023-04-06 16:02:16 +0700

    add: browser args

--- api/services/instagramService.js
+++ api/services/instagramService.js
@@ -42,7 +42,7 @@ const loadAllComments = async(page, post)=>{
   let responses = [];
 
   const [res] = await Promise.all([
-    page.waitForResponse(res => res.url().includes(apiPrefix), {timeout: 90000}),
+  page.waitForResponse(res => res.url().includes(apiPrefix), {timeout: 90000}),
    page.goto(post, {
         timeout: 0,
         waitUntil:"domcontentloaded"
@@ -129,7 +129,44 @@ exports.main = async (username, password, post) => {
   // - no default viewport (`defaultViewport: null` - website page will in full width and height)
   const browser = await puppeteer.launch({
     executablePath,
-    args: chromium.args,
+   args: [
+        ...chromium.args,
+        "--autoplay-policy=user-gesture-required",
+        "--disable-background-networking",
+        "--disable-background-timer-throttling",
+        "--disable-backgrounding-occluded-windows",
+        "--disable-breakpad",
+        "--disable-client-side-phishing-detection",
+        "--disable-component-update",
+        "--disable-default-apps",
+        "--disable-dev-shm-usage",
+        "--disable-domain-reliability",
+        "--disable-extensions",
+        "--disable-features=AudioServiceOutOfProcess",
+        "--disable-hang-monitor",
+        "--disable-ipc-flooding-protection",
+        "--disable-notifications",
+        "--disable-offer-store-unmasked-wallet-cards",
+        "--disable-popup-blocking",
+        "--disable-print-preview",
+        "--disable-prompt-on-repost",
+        "--disable-renderer-backgrounding",
+        "--disable-setuid-sandbox",
+        "--disable-speech-api",
+        "--disable-sync",
+        "--hide-scrollbars",
+        "--ignore-gpu-blacklist",
+        "--metrics-recording-only",
+        "--mute-audio",
+        "--no-default-browser-check",
+        "--no-first-run",
+        "--no-pings",
+        "--no-sandbox",
+        "--no-zygote",
+        "--password-store=basic",
+        "--use-gl=swiftshader",
+        "--use-mock-keychain",
+      ],
     headless: false,
     defaultViewport: null,
   });
